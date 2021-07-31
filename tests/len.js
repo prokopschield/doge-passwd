b2e5c5b3fc46cadf731484853d1526730626b629
Prokop <prokop.schield@protonmail.com>
2021-07-31 12:31:55 +0200

    0.1.0 Add `tests/len.js`

---
+++ tests/len.js
@@ -0,0 +1,18 @@
+const { hash, verify } = require('..');
+
+const passwd = 'foobar';
+
+module.exports = () => {
+	for (let i = 8; i <= 80; i += 8) {
+		let h = hash(passwd, i);
+		let l = h.length;
+		if (l !== i) {
+			console.log({ h });
+			throw new Error(`tests/len.js: hash of length ${i} returned length ${l}`);
+		}
+		if (!verify(passwd, h)) {
+			console.log({ h });
+			throw new Error(`tests/len.js: hash of length ${i} failed to verify`);
+		}
+	}
+};
